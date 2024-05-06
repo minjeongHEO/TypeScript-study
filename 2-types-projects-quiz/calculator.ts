9d87cb9e79ec2b1bcf0b7fbd1714e2dee62f5e1e
minjeongHEO <hmjoung9770@gmail.com>
2024-05-06 18:00:31 +0900

    feat: 3.3 계산기 함수 만들기 💡

--- 2-types-projects-quiz/calculator.ts
+++ 2-types-projects-quiz/calculator.ts
@@ -1,8 +1,29 @@
 /**
  * Let's make a calculator 🧮
  */
-console.log(calculate('add', 1, 3)); // 4
-console.log(calculate('substract', 3, 1)); // 2
-console.log(calculate('multiply', 4, 2)); // 8
-console.log(calculate('divide', 4, 2)); // 2
-console.log(calculate('remainder', 5, 2)); // 1
+{
+    type CalculateType =
+        | "add"
+        | "substract"
+        | "multiply"
+        | "divide"
+        | "remainder";
+
+    function calculate(
+        type: CalculateType,
+        CalculateNum1: number,
+        CalculateNum2: number
+    ) {
+        if (type === "add") return CalculateNum1 + CalculateNum2;
+        if (type === "substract") return CalculateNum1 - CalculateNum2;
+        if (type === "multiply") return CalculateNum1 * CalculateNum2;
+        if (type === "divide") return CalculateNum1 / CalculateNum2;
+        if (type === "remainder") return CalculateNum1 % CalculateNum2;
+    }
+
+    console.log(calculate("add", 1, 3)); // 4
+    console.log(calculate("substract", 3, 1)); // 2
+    console.log(calculate("multiply", 4, 2)); // 8
+    console.log(calculate("divide", 4, 2)); // 2
+    console.log(calculate("remainder", 5, 2)); // 1
+}
