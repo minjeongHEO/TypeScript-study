5b652529d7e22c8d4c1ee165796998586ba684d1
minjeongHEO <hmjoung9770@gmail.com>
2024-05-06 14:27:59 +0900

    feat: 2.7 함수 타입 이용 (spread, default, optional)

--- 1-types/1-2-functions.ts
+++ 1-types/1-2-functions.ts
@@ -22,4 +22,28 @@
             resolve(100);
         });
     }
+
+    //* Optional Parameter
+    // "?:" 인자를 전달해도 되고, 전달하지 않아도 된다.
+    function printName(firstName: string, lastName?: string) {
+        console.log(firstName, lastName);
+    }
+    printName("Steve", "Jobs");
+    printName("Ellie");
+    printName("Anna", undefined);
+
+    //* Default Parameter
+    function printMessage(message: string = "default message") {
+        console.log(message);
+    }
+    printMessage();
+
+    //* Rest Parameter
+    //인자의 개수 제한 없이 사용가능
+    function addNumbers(...numbers: number[]): number {
+        return numbers.reduce((a, b) => a + b);
+    }
+    console.log(addNumbers(1, 2));
+    console.log(addNumbers(1, 2, 3));
+    console.log(addNumbers(1, 2, 3, 4));
 }
